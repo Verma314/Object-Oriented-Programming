595eadaf01207161d3b6ced3f2286f00e1739c63
Aditya Verma <adityasinghverma314@gmail.com>
2017-08-19 23:02:10 +0530

    Variable Argument Lists

--- VarArgs.java
+++ VarArgs.java
@@ -1,1 +1,17 @@
-import java.i0.*;
+import static java.lang.System.*;
+class A {}
+
+class VarArgs {
+  static void printArray ( Object [] args ) {
+    for ( Object obj : args)
+     out.println(obj+" ");
+    out.println();
+    }
+  public static void main (String [] args ) {
+    printArray( new Object[] {new Integer(12), new Float(12.12), new Double(12.13)
+    } );
+    printArray( new Object[] {"one","two","three"} );
+    printArray( new Object[] {new A(), new A(), new A()});
+  }
+
+}
