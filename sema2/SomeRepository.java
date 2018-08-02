4cbf88cddce6b14779c93e9f10f9e33ab63eb67f
Aditya Verma <adityasinghverma314@gmail.com>
2018-08-02 11:58:37 +0530

    semaphores

---
+++ sema2/SomeRepository.java
@@ -0,0 +1,26 @@
+package sema2;
+
+import java.util.concurrent.Semaphore;
+public class SomeRepository {
+    int totalConnections = 10;
+    Semaphore semaphore = new Semaphore(7);
+
+    public void getConnection() throws Exception{
+        semaphore.acquire();
+        System.out.println("Permits available: " + semaphore.availablePermits());
+        if(totalConnections<=0){
+            throw new Exception("DB connection not availble");
+        }
+        totalConnections = totalConnections - 1;
+        System.out.println(Thread.currentThread().getName());
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("Got Connection!! Left: " +totalConnections);;
+        totalConnections = totalConnections + 1;
+        semaphore.release();
+    }
+}
---
+++ sema2/TestConnections.java
@@ -0,0 +1,35 @@
+package sema2;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+class MyRunnable implements Runnable{
+    SomeRepository repo;
+    public MyRunnable(SomeRepository repo) {
+        super();
+        this.repo = repo;
+    }
+    @Override
+    public void run() {
+//		System.out.println(Thread.currentThread().getName());
+        try {
+            repo.getConnection();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
+public class TestConnections {
+
+    public static void main(String[] args) {
+        SomeRepository repo = new SomeRepository();
+        ExecutorService service = Executors.newFixedThreadPool(20);
+        for(int i=0;i<20;i++){
+            MyRunnable runnable = new MyRunnable(repo);
+            service.submit(runnable);
+        }
+
+
+    }
+}
