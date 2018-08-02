ed7ace53e36dc64d3169b668ca446f40f7a3d255
Aditya Verma <adityasinghverma314@gmail.com>
2018-08-02 11:29:22 +0530

    Update TestSorting.java

--- functional programming in java/funcomapre/TestSorting.java
+++ functional programming in java/funcomapre/TestSorting.java
@@ -2,6 +2,8 @@ package funcomapre;
 import java.util.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class TestSorting {
     public  static void displayLists (ArrayList<Employee> employeeList) {
@@ -25,9 +27,9 @@ public class TestSorting {
         Collections.sort(employeeList, (e01,e02) -> e01.getLastName().compareTo(e02.lastName));
         displayLists(employeeList);
         //do conditions, similar to filter-ish
-        printConditionally(employeeList, (e) -> e.getLastName().startsWith("z")); //print last names starting with z
-
-
+        printConditionally(employeeList, (e) -> e.getLastName().startsWith("z"));
+        printConditionallyUsingLibrary(employeeList, (e) -> e.getLastName().startsWith("z"));
+        printConditionallyUsingLibrary(employeeList, (e) -> e.getLastName().startsWith("z"), (e) -> System.out.println(e.getLastName()));
     }
 
     public static void printConditionally(List<Employee> employees, Condition condition){
@@ -39,5 +41,25 @@ public class TestSorting {
         }
     }
 
+    //predicate is a built-in interface
+    public static void printConditionallyUsingLibrary(List<Employee> employees, Predicate<Employee> predicate,
+                                                      Consumer<Employee> consumer){
+        for(Employee e: employees){
+            if(predicate.test(e)){
+               consumer.accept(e); //consumer is an implementation of accept method
+            }
+
+        }
+    }
+
+
+    public static void printConditionallyUsingLibrary(List<Employee> employees, Predicate<Employee> predicate){
+        for(Employee e: employees){
+            if(predicate.test(e)){
+                System.out.println("\nsatisifying condition (lib) " + e.getLastName());
+            }
+
+        }
+    }
 
 }
